0e92bacc931bacb4dbf8cd8ecd77ec5ff0ed39b3
trungkhongbietcode <106616203+trungkhongbietcode@users.noreply.github.com>
2022-06-01 02:48:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "TrungDzz",
-    introTitle: 'Huyen Anh',
+    titleWeb: "Trung Đi Tỏ Tình Dạo",
+    introTitle: 'Cậu làm người iu tớ nha<333',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Dô facebook của mình nhắn tin nhaaa',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100050608237291' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
